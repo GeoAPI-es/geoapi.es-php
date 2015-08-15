cda60ec6b5619674f7eec1d844b7ad64f0778fc9
Alexander Nestorov <alexandernst@gmail.com>
2015-08-15 12:16:54 +0200

    Añadir comentarios

--- demo/GeoAPI_example.php
+++ demo/GeoAPI_example.php
@@ -9,15 +9,16 @@ $geoapi->setConfig("type", "JSON");
 $geoapi->setConfig("sandbox", 1);
 
 $geoapi->comunidades(array(
-	//
+	//Sin argumentos
 ))->then(function($v){
 	echo print_r($v, true);
 });
 
+//Esperar 1 segundo para evitar el limite de la API en sandbox
 sleep(1);
 
 $geoapi->provincias(array(
-	'CCOM' => '08'
+	'CCOM' => '08' //Provincias de "Castilla y León"
 ))->then(function($v){
 	echo print_r($v, true);
 });
